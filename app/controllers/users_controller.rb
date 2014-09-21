ea10db22efe176c990d7d955f007d6b239ef3f12
Iwark <iwark02@gmail.com>
2014-09-21 09:44:12 +0900

    ipaddress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    ip_address = request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
-    user = User.find_by(name: user_params[:name])
+    remoteaddr = 'unknown'
+
+    if request.env['HTTP_X_FORWARDED_FOR']
+      remoteaddr = request.env['HTTP_X_FORWARDED_FOR'].split(",")[0]
+    else
+      remoteaddr = request.env['REMOTE_ADDR'] if request.env['REMOTE_ADDR']
+    end
+    ip_address = remoteaddr
+    user = User.find_by(ip_address: remoteaddr)
     if user
       @user = user
     else
